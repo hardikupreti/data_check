5c35277adb9d8be6aa1a466903f50ad34073b6a4
hardikupreti <40667908+hardikupreti@users.noreply.github.com>
2022-11-11 14:19:53 +0530

    improved webcheck script

--- webcheck.py
+++ webcheck.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import logging as lg
+import requests
+from datetime import *
+
 
 lg.basicConfig(filename='data_check.log', format="%(asctime)s::%(levelname)s::%(lineno)d::%(message)s",
                filemode='w',force=True)
@@ -9,10 +12,12 @@ logger = lg.getLogger()
 
 logger.setLevel(lg.INFO)
 
-reference_file_path = r'C:\Users\upretihardik\Desktop\actual_reference_data.csv'
-
+reference_file_path = r'https://raw.githubusercontent.com/hardikupreti/data_check/main/actual_reference_data.csv'
+# can handle data form url or local path
+# df_out = pd.DataFrame(['starting logs\n-------------------------------------------------------'])
+lis = []
 def checkcsvfile_voltages(dataframe):
-    df_refpath = pd.read_csv(reference_file_path,encoding = 'unicode_escape')
+    df_refpath = pd.read_csv(reference_file_path,encoding = 'unicode_escape',index_col=0)
     voltages = df['VDDD'].unique()
     voltage_ref = df_refpath['VDDD'].unique()
     if set(voltages)==set(voltage_ref):
@@ -20,22 +25,53 @@ def checkcsvfile_voltages(dataframe):
     else:
         return False,voltages,voltage_ref
 
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv(sep = '\t',date_format='%Y-%m-%d %H:%M:%S').encode('utf-8')
+
+def timefunc():
+    return datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+t = timefunc()
 
 uploaded_file = st.file_uploader("Select a CSV File",type = ['csv'])
 if uploaded_file is not None:
 
-    logger.info('File Uploaded Successfully!')
+    lis.append(f'{t}::File Uploaded Successfully!')
     df = pd.read_csv(uploaded_file,encoding='unicode_escape')
 
 if st.button('Check Uploaded File'):
-    logger.info('Checking data')
+    lis.append(f'{t}::Checking data')
     result,volts,voltsref = checkcsvfile_voltages(df)
     if result:
-        logger.info('VOLTAGE CHECK : PASSED')
-        logger.info(f'the voltage values used are : {volts}')
+        lis.append(f'{t}::VOLTAGE CHECK : PASSED')
+        lis.append(f'{t}::the voltage values used are : {volts}')
+        df_out = pd.DataFrame(lis)
+        st.write(df_out)
+        # df_out.to_csv('final_log.log', header=False, index=False)
+        csv = convert_df(df_out)
+        st.download_button(
+            label="Download Result Datalog",
+            data=csv,
+            file_name='datalog_final.log',
+            mime='text/csv',
+        )
+
     else:
-        logger.warning('VOLTAGE CHECK: FAILED')
-        logger.info(f'The voltages in the master file are : {voltsref}')
-        logger.info(f'The voltages used in the given file are {volts}')
+        lis.append(f'{t}::VOLTAGE CHECK: FAILED')
+        lis.append(f'{t}::The voltages in the master file are : {voltsref}')
+        lis.append(f'{t}::The voltages used in the given file are {volts}')
+        df_out = pd.DataFrame(lis)
+        st.write(df_out)
+        # df_out.to_csv('final_log.log', header=False, index=False)
+        csv = convert_df(df_out)
+        st.download_button(
+            label="Download Result Datalog",
+            data=csv,
+            file_name='datalog_final.log',
+            mime='text/csv',
+        )
+
+
 
 
